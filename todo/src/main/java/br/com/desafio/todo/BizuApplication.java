77be0f253fa7f83b28c47e220a714ffe041ab7bb
Vitor de Paula Silveira <silveira_vitor@outlook.com>
2023-05-23 16:16:11 -0300

    Base - inicialização gerada pelo spring io, com dependencias de lombok e jpa para facilitar

---
+++ todo/src/main/java/br/com/desafio/todo/BizuApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizuApplication.class, args);
+	}
+
+}
---
+++ todo/src/test/java/br/com/desafio/todo/BizuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
